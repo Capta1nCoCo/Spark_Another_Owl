c66bf5e0ae9b4ee69e7237283d04e01dac0a3f4a
Capta1nCoCo <femabu28@gmail.com>
2020-07-04 17:11:48 +0300

    160. Display Health & Score

--- Spark Another Owl/Assets/Scripts/EnemySpawner.cs
+++ Spark Another Owl/Assets/Scripts/EnemySpawner.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] float secondsBetweenSpawns = 3f;
     [SerializeField] List<GameObject> enemiesToSpawn = new List<GameObject>();
+    [SerializeField] Text scoreText;
+    int enemyCounter = 0;    
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreText.text = enemyCounter.ToString();
         StartCoroutine(SpawnEnemies());
     }
 
@@ -19,7 +23,14 @@ public class EnemySpawner : MonoBehaviour
         {
             var spawnedEnemy = Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
             spawnedEnemy.transform.parent = gameObject.transform;
+            AddScore();
             yield return new WaitForSeconds(secondsBetweenSpawns);
-        }        
-    }   
+        }
+    }
+
+    private void AddScore()
+    {
+        enemyCounter++;
+        scoreText.text = enemyCounter.ToString();
+    }
 }
--- Spark Another Owl/Assets/Scripts/PlayerHealth.cs
+++ Spark Another Owl/Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int baseHitPoints = 10;
+    [SerializeField] int baseHP = 10;
     [SerializeField] int dmgPoints = 1;
+    [SerializeField] Text healthText;
+
+    private void Start()
+    {
+        healthText.text = baseHP.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        baseHitPoints = baseHitPoints - dmgPoints;
+        baseHP -= dmgPoints;
+        healthText.text = baseHP.ToString();
     }
 }
